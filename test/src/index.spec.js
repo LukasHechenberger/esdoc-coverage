cc31b8f5a3b9ead286efb354425b6ef1da3965b9
Lukas Hechenberger <admin@ls-age.com>
2017-04-09 20:43:46 +0200

    style: Fix invalid test annotation

--- test/src/index.spec.js
+++ test/src/index.spec.js
@@ -3,7 +3,7 @@ import ESDocCoverage from '../../src/index';
 
 /** @test {ESDocCoverage} */
 describe('ESDocCoverage', function() {
-  /** @test {EsdocCoverage.version} */
+  /** @test {ESDocCoverage.version} */
   describe('.version', function() {
     it('should return current package version', function() {
       expect(ESDocCoverage.version, 'to match', /[0-9]+\.[0-9]+\.[0-9]+/);
